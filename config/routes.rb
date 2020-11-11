f2389f27e78c22aa2b289aeb365e1daecada88ff
cantstopcoding <jonathan_koss@yahoo.com>
2020-11-11 17:09:12 -0500

    add namespace/v1/items route in config route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # namespace the routes
+  # namespacing is important because we'll want our backend routes to be associated with an api
+  # http://cdamdesigns.com/api to indicates that it's an api endpoint and not just a front end endpoint
+  # also good to indicate which versions you're working on i.e. http://cdamdesigns.com/api/v1/items
+  # source: https://github.com/AyanaZaire/seeda_syllabus_backend/blob/master/project-build-part2-notes.md
+  # source: https://www.youtube.com/watch?v=sLrFiwhMPZU&feature=youtu.be 20:50
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :items, only: [:index]
+    end
+  end
 end
