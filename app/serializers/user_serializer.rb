3a36938cf4577dc5ccc800db10d34e592f6af541
cantstopcoding <jonathan_koss@yahoo.com>
2020-11-22 16:50:20 -0500

    get rid of items in user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :first_name, :last_name, :username, :email, :image_url, :items
+  attributes :first_name, :last_name, :username, :email, :image_url
 end
