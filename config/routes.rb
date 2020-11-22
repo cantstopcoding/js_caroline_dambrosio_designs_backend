7fd587ebfd5a640f124dd5088b3b4f1f640f34d9
cantstopcoding <jonathan_koss@yahoo.com>
2020-11-22 10:13:33 -0500

    create users create route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     namespace :v1 do 
       resources :items, only: [:index, :create]
       resources :categories, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end
