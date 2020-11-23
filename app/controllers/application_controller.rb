99f33ea8de9a73a567ef48aebbb67b14cb87613d
cantstopcoding <jonathan_koss@yahoo.com>
2020-11-23 17:43:30 -0500

    create encode_token, auth_header and decode_token methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::API
-    
+    def encode_token(payload)
+        JWT.encode(payload, 'very_secret')
+    end
+
+    def auth_header
+        # { Authorization: 'Bearer <token>' }
+        request.headers['Authorization']
+    end
+
+    def decode_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'very_secret', true, algorithm: 'HS256')    
+            rescue => JWT:DecodeError
+                nil 
+            end
+        end
+
+
+    end
 end
