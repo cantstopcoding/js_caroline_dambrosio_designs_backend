7bdb25b88ec4e36da4c603c5c787ad1c1ff1da0d
cantstopcoding <jonathan_koss@yahoo.com>
2020-12-02 17:56:11 -0500

    seed data and make sure associations are working

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  has_many :reviews, dependent: :destroy
+  has_many :reviewed_users, through: :reviews, source: :user
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :item
   belongs_to :user
+
+  has_many :reviewed_users, through: :reviews, source: :user
 end
