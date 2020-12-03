b6e2d829dbd0ba5423035140829daa22d5b27082
cantstopcoding <jonathan_koss@yahoo.com>
2020-12-03 10:39:12 -0500

    define create action and strong params in reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::ReviewsController < ApplicationController
-    skip_before_action :authorized, only: [:index]
+    skip_before_action :authorized, only: [:index, :create]
 
     def index 
         reviews = Review.all 
         render json: ReviewSerializer.new(reviews)
+    end
+
+    def create
+        review = Review.new(review_params)
+        
+        if review.save 
+            render json: ReviewSerializer.new(review), status: :accepted
+        else
+            render json: {errors: review.errors.full_messages}, status: :unprocessible_entity 
+        end
+    end
+
+    private
+    
+    def review_params
+        params.require(:review).permit(:content, :item_id, :user_id)
     end
 end
