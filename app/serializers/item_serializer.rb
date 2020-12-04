0edf3b1623864b525bfed8f8ef84cde2c6780901
cantstopcoding <jonathan_koss@yahoo.com>
2020-12-04 00:23:05 -0500

    add review in items api

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,13 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :image_url, :price, :description, :category_id, :category 
+  attributes :name, :image_url, :price, :description, :category_id, :category, :reviews
+
+  def reviews
+    object.reviews.map do |review|
+      ::ReviewSerializer.new(review).attributes
+    end
+  end
 end
+
+
+
