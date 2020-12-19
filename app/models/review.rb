2a40015499ad0da52c41f0fbef76b9cea3a74787
cantstopcoding <jonathan_koss@yahoo.com>
2020-12-19 10:36:45 -0500

    change default created at attribute in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ApplicationRecord
   # belongs_to :user
 
   # has_many :reviewed_users, through: :reviews, source: :user
+
+  def created_at
+    attributes['created_at'].strftime("%m/%d/%Y")
+  end
 end
