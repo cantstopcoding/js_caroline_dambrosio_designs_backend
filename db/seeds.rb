a8936bef5888498afaeb334620a50e9f36d96dff
cantstopcoding <jonathan_koss@yahoo.com>
2020-12-27 15:19:49 -0500

    change seed data to picture that actually renders

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ sandals = Category.create(name: 'Sandals')
 
 outlander_dress  = Item.create(name: "Outlander Costume", image_url: "https://yt3.ggpht.com/a/AATXAJx5mEunDOUJudzpBNqD61pppw8kcQvgmDHMosko=s100-c-k-c0xffffffff-no-rj-mo", price: 500.00, description:"It's awesome!", category_id: dresses.id)
 expensive_purse = Item.create(name: "Expensive Purse", image_url: "https://cdn.shopify.com/s/files/1/0384/0161/products/56265-92_20Chanel_20Vintage_20Classic_20Double_20Flap_20Bag_20Qu_2D_0002_518x518.jpg?v=1591021463", price: 100000.00, description:"It's awesome!", category_id: purses.id)
-expensive_sandals = Item.create(name: "Expensive Sandals", image_url: "//images.neimanmarcus.com/ca/5/product_assets/X/4/E/E/1/NMX4EE1_mu.jpg", price: 5000000000000.54, description: "It's too much damn money", category_id: sandals.id)
+expensive_sandals = Item.create(name: "Expensive Sandals", image_url: "https://www.mybudapester.com/media/catalog/product/cache/2/image/900x900/9df78eab33525d08d6e5fb8d27136e95/i/m/image_3681_1_5394.jpg", price: 5000000000000.54, description: "It's too much damn money", category_id: sandals.id)
 
 coco_review = Review.create(content: "Nice Dress!", item_id: outlander_dress.id)
 christian_review = Review.create(content: "Wow!", item_id: expensive_purse.id)
