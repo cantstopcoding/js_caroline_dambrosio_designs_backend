f6b62cca40b8402fe7ac2006362239a336d6bf37
cantstopcoding <jonathan_koss@yahoo.com>
2021-06-09 17:38:24 -0400

    delete reviews method, not needed

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,10 +1,4 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :image_url, :price, :description, :category_id, :category, :reviews
-
-  def reviews
-    object.reviews.map do |review|
-      ::ReviewSerializer.new(review).attributes
-    end
-  end
 end
